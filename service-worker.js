c25e36a108849d1ae098334cc817b764f909a58e
Ihor Karpyn <igor82200@gmail.com>
2021-05-17 18:41:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.52c1f46edcaa9e6b86ad8b84d12e0dbb.js"
 );
 
 self.addEventListener('message', (event) => {
